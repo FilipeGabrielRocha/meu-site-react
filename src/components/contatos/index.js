4e8764bd1866ff823337710cdc6622b093e1de09
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-03-23 09:41:46 -0300

    adicionado novas informações na página de contatos

--- src/components/contatos/index.js
+++ src/components/contatos/index.js
@@ -10,24 +10,44 @@ const Contatos = () => {
     <div className="container-meu-contato">
       <div class="container-meus-contatos">
         <h1 className="titulo-meus-contatos">Meus Contatos</h1>
+        <p className="email-meus-contatos">
+          <span className="span-meus-contatos">E-mail:</span>
+          <br /> filiperochaprogramador@gmail.com
+        </p>
         <ul className="lista-meus-contatos">
           <li>
-            <Link className="meus-contatos github">
+            <Link
+              to={"https://github.com/FilipeGabrielRocha"}
+              target="_blank"
+              className="meus-contatos github"
+            >
               <FaGithub /> - Github
             </Link>
           </li>
           <li>
-            <Link className="meus-contatos instagram">
+            <Link
+              to={"https://www.instagram.com/_filiperochaa/"}
+              target="_blank"
+              className="meus-contatos instagram"
+            >
               <FaInstagram /> - Instagram
             </Link>
           </li>
           <li>
-            <Link className="meus-contatos linkedin">
+            <Link
+              to={"https://www.linkedin.com/in/filipe-gabriel-rocha-a4446516a/"}
+              target="_blank"
+              className="meus-contatos linkedin"
+            >
               <FaLinkedinIn /> - LinkedIn
             </Link>
           </li>
           <li>
-            <Link className="meus-contatos site">
+            <Link
+              to={"https://filipegabrielrocha.github.io/portfolio/"}
+              target="_blank"
+              className="meus-contatos site"
+            >
               <FaServer /> - Meu Portfolio
             </Link>
           </li>
