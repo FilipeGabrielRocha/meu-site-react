3403897f76459dbb054406b850259f59758ca141
Filipe Rocha <filiperochaprogramador@gmail.com>
2023-03-23 09:53:22 -0300

    arrumando o tamanho do header em telas maiores

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,18 +5,19 @@ import "./index.css";
 const Header = () => {
   return (
     <header>
-      <Link className="logo" to={"/"}>
-        <img src={logo} alt="Logo do Site" />
-      </Link>
-
-      <nav>
-        <ul>
-          <li className="bar-nav-link"><Link to={"/"} className="link">Home</Link></li>
-          <li className="bar-nav-link"><Link to={"/"} className="link">Sobre</Link></li>
-          <li className="bar-nav-link"><Link to={"/"} className="link">Meus Jogos</Link></li>
-          <li className="bar-nav-link contato"><Link to={"/contato"} className="link">Contato</Link></li>
-        </ul>
-      </nav>
+      <div className="container-header">
+        <Link className="logo" to={"/"}>
+          <img src={logo} alt="Logo do Site" />
+        </Link>
+        <nav>
+          <ul>
+            <li className="bar-nav-link"><Link to={"/"} className="link">Home</Link></li>
+            <li className="bar-nav-link"><Link to={"/"} className="link">Sobre</Link></li>
+            <li className="bar-nav-link"><Link to={"/"} className="link">Meus Jogos</Link></li>
+            <li className="bar-nav-link contato"><Link to={"/contato"} className="link">Contato</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
